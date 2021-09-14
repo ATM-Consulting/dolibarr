536b6f7dfca1445eb7e4127978c18a3431677539
Adrien Raze <adrien.raze@atm-consulting.fr>
2021-09-14 09:54:36 +0200

    FIX : changement de la mention CORE par B2B dans le fichier de génération des bon de prelevement xml

--- htdocs/compta/prelevement/class/bonprelevement.class.php
+++ htdocs/compta/prelevement/class/bonprelevement.class.php
@@ -2163,7 +2163,7 @@ class BonPrelevement extends CommonObject
 				$XML_SEPA_INFO .= '					<Cd>SEPA</Cd>'.$CrLf;
 				$XML_SEPA_INFO .= '				</SvcLvl>'.$CrLf;
 				$XML_SEPA_INFO .= '				<LclInstrm>'.$CrLf;
-				$XML_SEPA_INFO .= '					<Cd>CORE</Cd>'.$CrLf;
+				$XML_SEPA_INFO .= '					<Cd>B2B</Cd>'.$CrLf;
 				$XML_SEPA_INFO .= '				</LclInstrm>'.$CrLf;
 				$XML_SEPA_INFO .= '				<SeqTp>'.$format.'</SeqTp>'.$CrLf;
 				$XML_SEPA_INFO .= '			</PmtTpInf>'.$CrLf;
