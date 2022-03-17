f61020e06c7f8ba2964b60f43bf875e94dd00662
steve <stevebotella@hotmail.fr>
2022-03-17 11:53:52 +0100

    feat: sigrenea contact facturation

--- htdocs/core/modules/facture/doc/pdf_sigrenea_contact_facturation.modules.php
+++ htdocs/core/modules/facture/doc/pdf_sigrenea_contact_facturation.modules.php
@@ -2193,7 +2193,7 @@ class pdf_sigrenea_contact_facturation extends ModelePDFFactures
 	protected function _pagefoot(&$pdf, $object, $outputlangs, $hidefreetext = 0)
 	{
 		global $conf;
-		$showdetails = empty($conf->global->MAIN_GENERATE_DOCUMENTS_SHOW_FOOT_DETAILS) ? 0 : $conf->global->MAIN_GENERATE_DOCUMENTS_SHOW_FOOT_DETAILS;
+		$showdetails = 1; //Display adress
 		return pdf_pagefoot($pdf, $outputlangs, 'INVOICE_FREE_TEXT', $this->emetteur, $this->marge_basse, $this->marge_gauche, $this->page_hauteur, $object, $showdetails, $hidefreetext);
 	}
 
