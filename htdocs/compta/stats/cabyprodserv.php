cfde887b5cc9dd1d3cc05aa3ad4f9af400d8f85a
Irvine Fleith <irvine.fleith@atm-consulting.fr>
2024-10-01 16:25:30 +0200

    FIX : Arrondi

--- htdocs/compta/stats/cabyprodserv.php
+++ htdocs/compta/stats/cabyprodserv.php
@@ -503,7 +503,7 @@ if ($modecompta == 'CREANCES-DETTES') {
 
 			// Quantity
 			print '<td class="right">';
-			print price($qty[$key], 1, $langs, 0, 0);
+			print floatval($qty[$key]);
 			print '</td>';
 
 			// Percent;
