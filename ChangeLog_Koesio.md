db19e9725c3590e30540bebd50e33b4fbab0b3e6
Irvine Fleith <irvine.fleith@atm-consulting.fr>
2025-01-15 12:18:44 +0100

    feat(services-list): created global search; backported from develop

--- ChangeLog_Koesio.md
+++ ChangeLog_Koesio.md
@@ -1,4 +1,5 @@
 ## Backported from 17.0 :
+- Backport develop PR #32662 : New hooks in services_list and added global search
 - Backport develop PR #32129 : New field rule for lines dates
 - Backport 17.0 PR #32247 : added error message in cron
 - Backport develop : fix abs fatal error
--- htdocs/contrat/services_list.php
+++ htdocs/contrat/services_list.php
@@ -96,6 +96,21 @@ $extrafields->fetch_name_optionals_label($object->table_element);
 
 $search_array_options = $extrafields->getOptionalsFromPost($object->table_element, '', 'search_');
 
+/** BACKPORT PR 32662 */
+$sall = GETPOST('search_all', 'alphanohtml') ? GETPOST('search_all', 'alphanohtml') : GETPOST('sall', 'alphanohtml');
+
+$fieldstosearchall = [
+	'p.label' => 'Product',
+	's.nom' => 'ThirdParty',
+];
+$parameters = ['fieldstosearchall' => $fieldstosearchall];
+$reshook = $hookmanager->executeHooks('completeFieldsToSearchAll', $parameters, $object, $action);
+if ($reshook < 0) {
+	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
+}
+$fieldstosearchall = array_merge($fieldstosearchall, $hookmanager->resArray);
+/** END BACKPORT PR 32662 */
+
 // Security check
 $contratid = GETPOST('id', 'int');
 if (!empty($user->socid)) {
@@ -252,6 +267,13 @@ $sql .= " ".MAIN_DB_PREFIX."contratdet as cd";
 if (!empty($extrafields->attributes[$object->table_element]['label']) && is_array($extrafields->attributes[$object->table_element]['label']) && count($extrafields->attributes[$object->table_element]['label'])) {
 	$sql .= " LEFT JOIN ".MAIN_DB_PREFIX.$object->table_element."_extrafields as ef on (cd.rowid = ef.fk_object)";
 }
+/** BACKPORT PR 32662 */
+// Add table from hooks
+$parameters = [];
+$reshook = $hookmanager->executeHooks('printFieldListFrom', $parameters, $object); // Note that $action and $object may have been modified by hook
+$sql .= $hookmanager->resPrint;
+/** END BACKPORT PR 32662 */
+
 $sql .= " LEFT JOIN ".MAIN_DB_PREFIX."product as p ON cd.fk_product = p.rowid";
 if ($search_product_category > 0) {
 	$sql .= ' LEFT JOIN '.MAIN_DB_PREFIX.'categorie_product as cp ON cp.fk_product=cd.fk_product';
@@ -346,6 +368,13 @@ if (!empty($filter_opcloture) && $filter_opcloture != ' BETWEEN ' && $filter_opc
 if (!empty($filter_opcloture) && $filter_opcloture == ' BETWEEN ') {
 	$sql .= " AND cd.date_cloture ".$filter_opcloture." '".$db->idate($filter_datecloture_start)."' AND '".$db->idate($filter_datecloture_end)."'";
 }
+
+/** BACKPORT PR 32662 */
+if ($sall) {
+	$sql .= natural_search(array_keys($fieldstosearchall), $sall);
+}
+/** END BACKPORT PR 32662 */
+
 // Add where from extra fields
 include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_search_sql.tpl.php';
 $sql .= $db->order($sortfield, $sortorder);
@@ -392,6 +421,13 @@ if (!empty($contextpage) && $contextpage != $_SERVER["PHP_SELF"]) {
 if ($limit > 0 && $limit != $conf->liste_limit) {
 	$param .= '&limit='.$limit;
 }
+
+/** BACKPORT PR 32662 */
+if ($sall != '') {
+	$param .= '&sall='.urlencode($sall);
+}
+/** END BACKPORT PR 32662 */
+
 if ($search_contract) {
 	$param .= '&amp;search_contract='.urlencode($search_contract);
 }
