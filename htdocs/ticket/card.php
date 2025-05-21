ef5b83ac07b33cb409169be83b637256fbf72507
atm-corentin <corentin.carayon@atm-consulting.fr>
2025-05-21 14:15:53 +0200

    retour sur commentaire backport

--- htdocs/ticket/card.php
+++ htdocs/ticket/card.php
@@ -196,7 +196,7 @@ if (empty($reshook)) {
 		}
 		$action = 'view';
 	}
-	// backport : PR -FIX|Fix # issue when you try to attach of file on form ticket
+	// backport : PR #34227 - va être mergé en V20.0 ( à supprimer  lors du passge en V23.0 )
 	if (($action == 'add' && GETPOST('save', 'alpha') || ($action == 'update' && $object->status < Ticket::STATUS_CLOSED)) && $permissiontoadd) {
 		$ifErrorAction = ($action == 'add' ? 'create' : 'edit');	// Test on permission not required here
 		if ($action == 'add') {		// Test on permission already done
