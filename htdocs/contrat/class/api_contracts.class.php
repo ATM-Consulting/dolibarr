e07c5d80574197a5a6700b52c3c494a5a21e1abc
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2025-09-04 15:01:33 +0200

    comment

--- htdocs/contrat/class/api_contracts.class.php
+++ htdocs/contrat/class/api_contracts.class.php
@@ -550,9 +550,12 @@ class Contracts extends DolibarrApi
 			$result = $this->get($id);
 			unset($result->line);
 			return $this->_cleanObjectDatas($result);
-		} else {
+		}
+		//BACKPORT STD PR 35172
+		else {
 			throw new RestException(500, implode(';', $this->contract->errors));
 		}
+		// FIN BACKPORT STD PR 35172
 	}
 
 	/**
