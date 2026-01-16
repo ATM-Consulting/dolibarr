201cc7a8fb772b69fe16625d2070ecddb97dc7f3
Your Name <you@example.com>
2026-01-16 10:46:05 +0100

    backport : https://github.com/Dolibarr/dolibarr/pull/36898

--- htdocs/bom/class/bom.class.php
+++ htdocs/bom/class/bom.class.php
@@ -334,6 +334,7 @@ class BOM extends CommonObject
 		unset($object->id);
 		unset($object->fk_user_creat);
 		unset($object->import_key);
+		unset($object->date_creation);
 
 		// Clear fields
 		$default_ref = $this->fields['ref']['default'] ?? null;
