edec15dbab9ebbe397343ecc7e3f7c06402325cb
ATM-Lucas <lucas.mantegari@atm-consulting.fr>
2026-02-04 15:40:00 +0100

    retours PR

--- htdocs/comm/card.php
+++ htdocs/comm/card.php
@@ -999,7 +999,8 @@ if ($object->id > 0) {
 						}
 					}
 					$relativepath = dol_sanitizeFileName($objp->ref).'/'.dol_sanitizeFileName($objp->ref).'.pdf';
-					print $formfile->showPreview($file_list, $commande_static->element, $relativepath, 0, '&entity=' . $objp->entity);
+					$param .= '&entity=' . intval($objp->entity);
+					print $formfile->showPreview($file_list, $commande_static->element, $relativepath, 0, $param);
 				}
 				// $filename = dol_sanitizeFileName($objp->ref);
 				// $filedir = $conf->order->multidir_output[$objp->entity].'/'.dol_sanitizeFileName($objp->ref);
@@ -1093,7 +1094,8 @@ if ($object->id > 0) {
 						}
 					}
 					$relativepath = dol_sanitizeFileName($objp->ref).'/'.dol_sanitizeFileName($objp->ref).'.pdf';
-					print $formfile->showPreview($file_list, $sendingstatic->element, $relativepath, 0, '&entity=' . $objp->entity);
+					$param .= '&entity=' . intval($objp->entity);
+					print $formfile->showPreview($file_list, $sendingstatic->element, $relativepath, 0, $param);
 				}
 				// $filename = dol_sanitizeFileName($objp->ref);
 				// $filedir = $conf->expedition->multidir_output[$objp->entity].'/'.dol_sanitizeFileName($objp->ref);
