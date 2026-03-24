066619770cbe012cd98ed822737b7b835f7402ec
atm-nicolasV <nicolas.vidal@atm-consulting.fr>
2026-03-24 16:01:48 +0100

    removed FACTURE_ENABLE_NEGATIVE_LINES for credit note in facture.class.php

--- htdocs/compta/facture/class/facture.class.php
+++ htdocs/compta/facture/class/facture.class.php
@@ -4651,6 +4651,7 @@ class Facture extends CommonInvoice
 				$this->line->rang = $rangmax + 1;
 			}
 			$apply_abs_price_on_credit_note=false;
+			// ------------- spé acropose DA027835 ---------
 			if ($this->type == self::TYPE_CREDIT_NOTE && !getDolGlobalInt('INVOICE_KEEP_DISCOUNT_LINES_AS_IN_ORIGIN')) {
 				$apply_abs_price_on_credit_note = true;
 			}
