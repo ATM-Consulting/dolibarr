f2de8413fff068019ba92d0bb5326dbf71900045
atm-vincent <vincent.penel@atm-consulting.fr>
2026-04-02 15:13:35 +0200

    backport list contact on ticket

--- htdocs/api/class/api_documents.class.php
+++ htdocs/api/class/api_documents.class.php
@@ -458,7 +458,9 @@ class Documents extends DolibarrApi
 			}
 
 			$upload_dir = $conf->expensereport->dir_output.'/'.dol_sanitizeFileName($object->ref);
-		} elseif ($modulepart == 'ticket') {
+		}
+		// Backport V24 START - aac2ff7b
+		elseif ($modulepart == 'ticket') {
 			require_once DOL_DOCUMENT_ROOT.'/ticket/class/ticket.class.php';
 
 			if (!DolibarrApiAccess::$user->hasRight('ticket', 'read')) {
@@ -472,7 +474,9 @@ class Documents extends DolibarrApi
 			}
 
 			$upload_dir = $conf->ticket->dir_output.'/'.dol_sanitizeFileName((string) $object->ref);
-		} elseif ($modulepart == 'knowledgemanagement') {
+		}
+		// Backport V24 FIN - aac2ff7b
+		elseif ($modulepart == 'knowledgemanagement') {
 			require_once DOL_DOCUMENT_ROOT.'/knowledgemanagement/class/knowledgerecord.class.php';
 
 			if (!DolibarrApiAccess::$user->rights->knowledgemanagement->knowledgerecord->read && !DolibarrApiAccess::$user->rights->knowledgemanagement->knowledgerecord->read) {
